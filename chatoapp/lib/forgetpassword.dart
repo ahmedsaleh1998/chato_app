7ec2112bdc0d5830f4b896cdb21089cee574bad7
ahmedsaleh1998 <am304844@gmail.com>
2021-04-28 20:50:42 +0200

    signup and forget password
    this commit contain forget password and sign up.

--- chatoapp/lib/forgetpassword.dart
+++ chatoapp/lib/forgetpassword.dart
@@ -1,28 +1,27 @@
 import 'dart:ui';
 
-import 'package:chato_app/forgetpassword.dart';
 import 'package:chato_app/signup.dart';
 import 'package:flutter/material.dart';
 import 'package:chato_app/w_and_h.dart';
 
-class Login extends StatefulWidget {
+class forgetpass extends StatefulWidget {
   @override
-  _LoginState createState() => _LoginState();
+  _forgetpassState createState() => _forgetpassState();
 }
 
-class _LoginState extends State<Login> {
+class _forgetpassState extends State<forgetpass> {
   final _formKey = GlobalKey<FormState>();
   GlobalKey scafolindexkey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     TextEditingController email = TextEditingController();
-    TextEditingController password = TextEditingController();
+
     String e_mail = "";
-    String pass_word = "";
+
     var padingright = (get_width(context) - get_width(context) / 1.1) / 2;
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'index',
+      title: 'forgetpassword',
       home: Scaffold(
         key: scafolindexkey,
         body: Container(
@@ -43,13 +42,14 @@ class _LoginState extends State<Login> {
                       children: [
                         Padding(
                           padding: const EdgeInsets.only(top: 10),
-                          child: Text('Login', style: TextStyle(fontSize: 30)),
+                          child: Text('forget password',
+                              style: TextStyle(fontSize: 30)),
                         ),
                         SizedBox(
                           width: get_width(context),
                           height: get_height(context) / 2.5,
-                          child:
-                              Image.asset("assets/login.png", fit: BoxFit.fill),
+                          child: Image.asset("assets/forget.png",
+                              fit: BoxFit.fill),
                         ),
                         TextFormField(
                             controller: email,
@@ -69,29 +69,6 @@ class _LoginState extends State<Login> {
                               }
                               return null;
                             }),
-                        SizedBox(
-                          width: get_width(context) / 4,
-                          height: get_height(context) / 30,
-                        ),
-                        /////////////////////////////
-                        TextFormField(
-                            controller: password,
-                            decoration: InputDecoration(
-                              prefixIcon: Icon(Icons.lock),
-                              labelText: 'password',
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(20),
-                              ),
-                              disabledBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(20)),
-                            ),
-                            validator: (value) {
-                              if (value.isEmpty || value == "") {
-                                return 'this field is required';
-                              }
-                              return null;
-                            }),
-
                         ///////////////////////////////
                         SizedBox(
                           width: get_width(context) / 4,
@@ -101,7 +78,7 @@ class _LoginState extends State<Login> {
                           width: get_width(context) / 1.1,
                           height: get_height(context) / 15,
                           child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            mainAxisAlignment: MainAxisAlignment.center,
                             children: [
                               SizedBox(
                                 width: get_width(context) / 2,
@@ -111,12 +88,11 @@ class _LoginState extends State<Login> {
                                     if (_formKey.currentState.validate()) {
                                       setState(() {
                                         e_mail = email.text;
-                                        pass_word = password.text;
                                       });
                                     }
                                   },
                                   child: Text(
-                                    'Login',
+                                    'Reset Password',
                                     style: TextStyle(
                                         fontSize: 20, color: Colors.white),
                                   ),
@@ -126,48 +102,9 @@ class _LoginState extends State<Login> {
                                   color: Color.fromRGBO(55, 79, 71, 1.0),
                                 ),
                               ),
-                              SizedBox(
-                                width: get_width(context) / 3,
-                                height: get_height(context) / 15,
-                                child: RaisedButton(
-                                  onPressed: () {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) => Signup()));
-                                  },
-                                  child: Text(
-                                    'Sign up',
-                                    style: TextStyle(fontSize: 15),
-                                  ),
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(20)),
-                                ),
-                              ),
                             ],
                           ),
                         ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 30),
-                          child: Row(
-                            children: [
-                              Text('you alrady have account ? '),
-                              SizedBox(
-                                child: TextButton(
-                                  onPressed: () {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) =>
-                                                forgetpass()));
-                                  },
-                                  child: Text('forget password',
-                                      style: TextStyle(fontSize: 15)),
-                                ),
-                              ),
-                            ],
-                          ),
-                        )
                       ],
                     ),
                   ),
--- chatoapp/lib/signup.dart
+++ chatoapp/lib/signup.dart
@@ -1,30 +1,35 @@
 import 'dart:ui';
 
 import 'package:chato_app/forgetpassword.dart';
-import 'package:chato_app/signup.dart';
+import 'package:chato_app/login.dart';
 import 'package:flutter/material.dart';
 import 'package:chato_app/w_and_h.dart';
+import 'package:flutter/services.dart';
 
-class Login extends StatefulWidget {
+class Signup extends StatefulWidget {
   @override
-  _LoginState createState() => _LoginState();
+  _SignupState createState() => _SignupState();
 }
 
-class _LoginState extends State<Login> {
-  final _formKey = GlobalKey<FormState>();
-  GlobalKey scafolindexkey = GlobalKey<ScaffoldState>();
+class _SignupState extends State<Signup> {
+  final _formKey_signup = GlobalKey<FormState>();
+  GlobalKey scafoldsignupkey = GlobalKey<ScaffoldState>();
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+  TextEditingController username = TextEditingController();
+  TextEditingController phonenumber = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    TextEditingController email = TextEditingController();
-    TextEditingController password = TextEditingController();
     String e_mail = "";
     String pass_word = "";
+    String user_name = "";
+    num phone_number = 0;
     var padingright = (get_width(context) - get_width(context) / 1.1) / 2;
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'index',
       home: Scaffold(
-        key: scafolindexkey,
+        key: scafoldsignupkey,
         body: Container(
           width: get_width(context) / 1.1,
           height: get_height(context),
@@ -35,27 +40,27 @@ class _LoginState extends State<Login> {
                 child: Theme(
                   data: new ThemeData(
                     primaryColor: Color.fromRGBO(55, 79, 71, 1.0),
-                    primaryColorDark: Colors.red,
+                    primaryColorDark: Colors.yellow,
                   ),
                   child: Form(
-                    key: _formKey,
+                    key: _formKey_signup,
                     child: Column(
                       children: [
                         Padding(
                           padding: const EdgeInsets.only(top: 10),
-                          child: Text('Login', style: TextStyle(fontSize: 30)),
+                          child: Text('SignUp', style: TextStyle(fontSize: 30)),
                         ),
                         SizedBox(
                           width: get_width(context),
                           height: get_height(context) / 2.5,
-                          child:
-                              Image.asset("assets/login.png", fit: BoxFit.fill),
+                          child: Image.asset("assets/signup.png",
+                              fit: BoxFit.fill),
                         ),
                         TextFormField(
-                            controller: email,
+                            controller: username,
                             decoration: InputDecoration(
                               prefixIcon: Icon(Icons.email),
-                              labelText: 'E-mail',
+                              labelText: 'User name',
                               border: OutlineInputBorder(
                                 borderRadius: BorderRadius.circular(20),
                               ),
@@ -69,6 +74,58 @@ class _LoginState extends State<Login> {
                               }
                               return null;
                             }),
+                        SizedBox(
+                          width: get_width(context) / 4,
+                          height: get_height(context) / 30,
+                        ),
+                        /////////////////////////////
+                        TextFormField(
+                            controller: email,
+                            decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.lock),
+                              labelText: 'E-mail',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              disabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20)),
+                            ),
+                            validator: (value) {
+                              if (value.isEmpty || value == "") {
+                                return 'this field is required';
+                              }
+                              return null;
+                            }),
+
+                        //////////////////////////////////
+                        SizedBox(
+                          width: get_width(context) / 4,
+                          height: get_height(context) / 30,
+                        ),
+                        /////////////////////////////
+                        TextFormField(
+                            keyboardType: TextInputType.number,
+                            inputFormatters: <TextInputFormatter>[
+                              FilteringTextInputFormatter.digitsOnly
+                            ],
+                            controller: phonenumber,
+                            decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.lock),
+                              labelText: 'phone number',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              disabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20)),
+                            ),
+                            validator: (value) {
+                              if (value.isEmpty || value == "") {
+                                return 'this field is required';
+                              }
+                              return null;
+                            }),
+
+                        /////////////////////////////////
                         SizedBox(
                           width: get_width(context) / 4,
                           height: get_height(context) / 30,
@@ -91,11 +148,12 @@ class _LoginState extends State<Login> {
                               }
                               return null;
                             }),
+                        ////////////////////////
 
                         ///////////////////////////////
                         SizedBox(
                           width: get_width(context) / 4,
-                          height: get_height(context) / 30,
+                          height: get_height(context) / 25,
                         ),
                         SizedBox(
                           width: get_width(context) / 1.1,
@@ -108,15 +166,19 @@ class _LoginState extends State<Login> {
                                 height: get_height(context) / 15,
                                 child: RaisedButton(
                                   onPressed: () {
-                                    if (_formKey.currentState.validate()) {
+                                    if (_formKey_signup.currentState
+                                        .validate()) {
                                       setState(() {
                                         e_mail = email.text;
                                         pass_word = password.text;
+                                        user_name = username.text;
+                                        phone_number =
+                                            int.parse(phonenumber.text);
                                       });
                                     }
                                   },
                                   child: Text(
-                                    'Login',
+                                    'Sign Up',
                                     style: TextStyle(
                                         fontSize: 20, color: Colors.white),
                                   ),
@@ -134,10 +196,10 @@ class _LoginState extends State<Login> {
                                     Navigator.push(
                                         context,
                                         MaterialPageRoute(
-                                            builder: (context) => Signup()));
+                                            builder: (context) => Login()));
                                   },
                                   child: Text(
-                                    'Sign up',
+                                    'Login',
                                     style: TextStyle(fontSize: 15),
                                   ),
                                   shape: RoundedRectangleBorder(
